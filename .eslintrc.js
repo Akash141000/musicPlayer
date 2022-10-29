327ce0344dd876a76816d060e38c594b78bb46e6
akashpadwal <akashpadwal21@gmail.com>
2022-10-29 12:34:52 +0530

    initial commit - * prettier setup done * eslint setup done * husky setup done

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        quotes: ['error', 'single'],
+        'no-unused-declaration': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-empty-function': 'error',
+        'no-unsafe-optional-chaining': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'interface',
+            format: ['PascalCase'],
+            prefix: ['I'],
+          },
+        ],
+      },
+    },
+  ],
+};
